348e34bd65a59487213aa50d30e0688170e418cc
VitoScalleta17 <157980902+VitoScalleta17@users.noreply.github.com>
2024-08-11 12:54:06 +0530

    Updating Indentation 1Hello World.asm

--- 1Hello World.asm
+++ 1Hello World.asm
@@ -1,7 +1,7 @@
 .data
   myString:  .asciiz  "Hello World\n"  #Creating a String
   
- .text
+.text
   li $v0, 4                           #Preparing system to print 
   la $a0, myString                    #Loading the value
   syscall
